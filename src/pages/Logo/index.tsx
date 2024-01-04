3d32a8c43d47acc0c020aa66e8a832dcd02fba92
drafish <xwlyy1991@163.com>
2024-01-04 09:06:54 +0800

    add pages

---
+++ src/pages/Logo/index.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useAppDispatch } from '../../redux/hooks';
+import logo from '../../assets/logo-background.svg';
+import './index.css';
+
+const LogoPage: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch({ type: 'remixide/connect' });
+  }, []);
+
+  return (
+    <div>
+      <div className="remixLogo">
+        <img src={logo} />
+      </div>
+    </div>
+  );
+};
+
+export default LogoPage;
